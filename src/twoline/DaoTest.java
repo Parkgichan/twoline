8efe8732310e3233bf122ff3208462788f74ca2a
rlcks <rlcks@DESKTOP-K7FGF7G>
2021-05-18 23:13:35 +0900

    ㅁㄴㅇ

---
+++ src/twoline/DaoTest.java
@@ -0,0 +1,26 @@
+package twoline;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class DaoTest {
+	
+	public static void main(String[] args) {
+		OnelineDAO dao = new OnelineDAO();
+		OnelineDTO dto = new OnelineDTO();
+		
+		dto.setMemo("TestTest");
+		dao.insert(dto);
+		
+		ArrayList<OnelineDTO> dtos = dao.getList();
+		for(int i = 0 ; i < dtos.size(); i ++) {
+			OnelineDTO dt = dtos.get(i);
+			System.out.println("no : " + dt.getNo() +",memo: " + dt.getMemo()+ ",date: " +dt.getWdate());
+		}
+		
+	}
+}
